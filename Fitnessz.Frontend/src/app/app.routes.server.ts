a6df2018a1c8909f54822c39d4d12a98231d4256
Farkas Csaba <farkascsabi123@gmail.com>
2026-02-23 12:59:33 +0100

    Fix routing and budget errors for vercel deployment

--- Fitnessz.Frontend/src/app/app.routes.server.ts
+++ Fitnessz.Frontend/src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'kategoria/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'posztszerkesztes/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'egeszthread/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
--- Fitnessz.Frontend/src/app/app.routes.ts
+++ Fitnessz.Frontend/src/app/app.routes.ts
@@ -9,11 +9,12 @@ import {CreateThread} from './community-pages/create-thread/create-thread';
 import {authGuard} from './guards/auth-guard';
 import {EditThread} from './community-pages/edit-thread/edit-thread';
 import {editGuard} from './guards/edit-guard';
+import {RenderMode} from '@angular/ssr';
 
 
 export const routes: Routes = [
   { path: '', component: Explore},
-  { path: 'kategoria/:id', component: Categories},
+  { path: 'kategoria/:id',component: Categories,},
   { path: 'egeszthread/:id', component: Fullthread},
   { path: 'bejelentkezes', component: Login},
   { path: 'regisztralas', component: Register},
